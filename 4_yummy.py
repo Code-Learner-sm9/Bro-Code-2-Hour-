491819ae892ebf1821c94f192cdbb70fe913d6ac
Your Name <your.email@example.com>
2025-05-24 18:14:16 +0600

    yummy

---
+++ 4_yummy.py
@@ -0,0 +1,19 @@
+# Variable = A container for a value (String, integer, boolean, float)
+
+# Strings (A Series of Characters) In a string bruh123 are also 
+# recognized as string. Everything within a double quote is a string.
+first_name = "Labid"
+email = "bruh123@fake.com"
+print(first_name)
+
+#print using f-string (f for format)
+print(f"Heelo {first_name}")
+# Multiple 
+print(f"{first_name} Email is: {email} {first_name}")
+
+
+#Integers
+age = 27
+num = 30
+print(f"You are {age} years old")
+print(f"There are {num} students in our class")
